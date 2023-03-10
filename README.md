251101256d14a994f590e8f25ebbcb81e6edac4e
Andrew Petro <andrew.petro@wisc.edu>
2023-03-09 19:18:27 -0600

    Initial implementation

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
 # myuw-widget-launch-button
+
 Launch button for a MyUW widget. Intended as replacement for the launch-button AngularJS Directive in uportal-app-framework.
+
+This launch button is intended for use as part of a widget card.
+
+## Getting Started
+
+Include the web component as follows:
+
+```html
+<!-- import the module -->
+<script type="module" src="https://unpkg.com/@myuw-web-components/myuw-widget-launch-button@latest/dist/myuw-widget-launch-button.min.mjs"></script>
+
+
+<!-- Use the Web Component, probably in the context of a widget card. -->
+<myuw-widget-launch-button
+  data-myuw-url="https://enroll.wisc.edu/"
+  data-myuw-open-in-new-tab=true
+  data-myuw-text="Launch full app"
+  data-myuw-aria-label="Launch Course Search and Enroll"
+></myuw-widget-launch-button>
+```
+
+_Note:_ The evergreen "latest" version can be used for convenience, but in production settings it is recommended to use the latest [release version](https://github.com/myuw-web-components/myuw-widget-launch-button/releases) specifically, and upgrade only after testing!
+
+## Usage
+
+### Attributes
+
+#### data-myuw-url
+
+The `data-myuw-url` attribute tells the web component where the link should take the user.
+
+Required.
+
+#### data-myuw-open-in-new-tab
+
+The `data-myuw-open-in-new-tab` attribute tells the web component whether the link should open in the same tab or in a new tab.
+
+#### data-myuw-text
+
+The text label for display on the button.
+
+#### data-myuw-aria-label
+
+Aria label for the button.
+
+This is intended to make the launch button useful for screen reader users navigating by stepping through the hyperlinks on the page. Whereas a label like "Launch full app" does not clarify *which* app will be launched, "Launch Course Search and Enroll" communicates what app the link will launch.
+
+
+## Development and contribution
+
+To run the demo app locally and test the component, run the following commands:
+
+```bash
+$ npm install
+$ npm start
+```
---
+++ src/myuw-widget-launch-button.js
@@ -0,0 +1,67 @@
+import templateHtml from './myuw-widget-launch-button.html';
+
+class MyUWWidgetLaunchButton extends HTMLElement {
+
+  constructor() {
+    super();
+    this.attachShadow({mode: 'open'});
+    this.shadowRoot.appendChild(this.constructor.template.content.cloneNode(true));
+    this.launchButtonElement = this.shadowRoot.getElementById('myuw-widget-launch-button');
+  }
+
+  static get observedAttributes() {
+    return [
+      'data-myuw-url',
+      'data-myuw-open-in-new-tab',
+      'data-myuw-text',
+      'data-myuw-aria-label']
+  }
+
+
+  static get template() {
+    if (this._template === undefined) {
+      this._template = document.createElement('template');
+      this._template.innerHTML = templateHtml
+    }
+    return this._template;
+  }
+
+  /**
+  * Web component lifecycle hook to update changed properties
+  */
+  attributeChangedCallback(name, oldValue, newValue) {
+    // Update the attribute internally
+    this[name] = newValue;
+    // Update the component
+    this.updateComponent();
+  }
+
+  /**
+  * When component is first attached to the DOM,
+  * get its defined attributes and set up listeners
+  */
+  connectedCallback() {
+    // Get all attributes
+    this['data-myuw-url'] = this.getAttribute('data-myuw-url');
+    this['data-myuw-open-in-new-tab']         = this.getAttribute('data-myuw-open-in-new-tab');
+    this['data-myuw-text']         = this.getAttribute('data-myuw-text');
+    this['data-myuw-aria-label']         = this.getAttribute('data-myuw-aria-label');
+
+    this.$link = this.shadowRoot.getElementById('myuw-widget-launch-button');
+  }
+
+  /**
+  * Update the component state
+  */
+  updateComponent() {
+    this.launchButtonElement.innerHTML = this['data-myuw-text'];
+    this.launchButtonElement.setAttribute("href", this['data-myuw-url']);
+    if (this['data-myuw-url'] && this['data-myuw-url'].startsWith("http")) {
+      this.launchButtonElement.setAttribute("rel", "noopener noreferrer");
+      this.launchButtonElement.setAttribute("target", "_blank");
+    }
+  }
+
+}
+
+window.customElements.define('myuw-widget-launch-button', MyUWWidgetLaunchButton);
