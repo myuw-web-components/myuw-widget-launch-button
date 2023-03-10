f8542380d184f80ac8e8276937bd9565928c96c9
Andrew Petro <andrew.petro@wisc.edu>
2023-03-09 19:17:46 -0600

    bootstrap development environment

---
+++ rollup.config.js
@@ -0,0 +1,73 @@
+import html from 'rollup-plugin-html';
+import minify from 'rollup-plugin-minify-es';
+import babel from 'rollup-plugin-babel';
+
+let fileName = 'myuw-widget-launch-button';
+let objName = 'MyUWWidgetLaunchButton';
+
+let plugins = {
+  full: [
+    html({
+      include: `src/*.html`,
+      htmlMinifierOptions: {
+        collapseWhitespace: true,
+        collapseBooleanAttributes: true,
+        conservativeCollapse: true
+      }
+    })
+  ],
+  min: [
+    html({
+      include: `src/*.html`,
+      htmlMinifierOptions: {
+        collapseWhitespace: true,
+        collapseBooleanAttributes: true,
+        conservativeCollapse: true
+      }
+    }),
+    minify({
+      output: {
+        wrap_iife: true
+      }
+    })
+  ]
+};
+
+export default [
+  {
+    input: `src/${fileName}.js`,
+    plugins: plugins.full.concat([babel({exclude: 'node_modules/**'})]),
+    output: {
+      file: `dist/${fileName}.js`,
+      name: objName,
+      format: 'iife'
+    }
+  },
+  {
+    input: `src/${fileName}.js`,
+    plugins: plugins.min.concat([babel({exclude: 'node_modules/**'})]),
+    output: {
+      file: `dist/${fileName}.min.js`,
+      name: objName,
+      format: 'iife'
+    }
+  },
+  {
+    input: `src/${fileName}.js`,
+    plugins: plugins.full,
+    output: {
+      file: `dist/${fileName}.mjs`,
+      name: objName,
+      format: 'es'
+    }
+  },
+  {
+    input: `src/${fileName}.js`,
+    plugins: plugins.min,
+    output: {
+      file: `dist/${fileName}.min.mjs`,
+      name: objName,
+      format: 'es'
+    }
+  },
+];
